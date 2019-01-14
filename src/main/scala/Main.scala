7b30eef257ece34f5b7f35a1f85f40423c73af52
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2019-01-14 13:29:51 +0200

    Fix starting http server

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,13 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.google.inject.Guice
-import guice.bindings.AkkaBinding
+import guice.bindings.{AkkaBinding, MonixBinding}
 import net.codingwell.scalaguice.InjectorExtensions._
-import routes.graphql.GraphQLRoutes.routes
+import routes.graphql.GraphQLRoutes
 
 object Main extends App {
-  val injector = Guice.createInjector(new AkkaBinding())
+  val injector = Guice.createInjector(new AkkaBinding(), new MonixBinding())
   implicit val system: ActorSystem = injector.instance[ActorSystem]
   implicit val materializer: ActorMaterializer = injector.instance[ActorMaterializer]
-  Http().bindAndHandle(routes, "localhost", 9000)
+  Http().bindAndHandle(injector.instance[GraphQLRoutes].routes, "localhost", 9000)
 }
--- src/main/scala/routes/graphql/GraphQLRoutes.scala
+++ src/main/scala/routes/graphql/GraphQLRoutes.scala
@@ -2,8 +2,11 @@ package routes.graphql
 
 import akka.http.scaladsl.server.Directives.{get, getFromResource, pathSingleSlash}
 import akka.http.scaladsl.server.Route
+import com.google.inject.Inject
+import graphql.GraphQL
 
-object GraphQLRoutes {
+class GraphQLRoutes @Inject()(graphQL: GraphQL,
+                              webSocketHandler: WebSocketHandler) {
   val routes: Route = pathSingleSlash {
     get {
       getFromResource("web/graphiql.html")
